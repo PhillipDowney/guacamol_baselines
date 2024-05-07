8f7373cf12d03e93578775733841f4cbe28661af
Phil Downey <Phil.downey1@ibm.com>
2024-05-08 09:16:15 +1000

    Update vae_distribution_learning.py

--- guacamol_baselines/moses_baselines/vae_distribution_learning.py
+++ guacamol_baselines/moses_baselines/vae_distribution_learning.py
@@ -27,7 +27,7 @@ def get_parser():
 class VaeGenerator(DistributionMatchingGenerator):
     def __init__(self, config):
         model_config = torch.load(config.config_load)
-        map_location = 'cpu' if config.device == 'cpu' else 'cuda'
+        map_location = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model_state = torch.load(config.model_load, map_location=map_location)
         self.model_vocab = torch.load(config.vocab_load)
         self.config = config
